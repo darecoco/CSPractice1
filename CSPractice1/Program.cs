bb89b1398c0ed36f56c8d3f3e24eccfd48f7b3e4
Darecoco <whaqlrh37@naver.com>
2024-06-25 10:09:39 +0900

    #1 12-2 출력

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -11,15 +11,20 @@ namespace CSPractice1
         static void Main(string[] args)
         {
             //컨텍스트 키워드
-            var abc = "Hello World";
+            /* var abc = "Hello World";
             int var = 10; // 예약어가... 된다?
             Console.WriteLine(var); //함쌤 왈 : 갑자기 풀렸는데 쓰지 말자.
+            */
 
             // 한줄 주석
             /*
              여러줄 주석
              */
             /*** XML 주석 (문서화 주석) ***/
+
+            //출력
+            Console.WriteLine("Hello World");
+            Console.Write("뉴라인(줄바꿈)x");
         }
     }
 }
