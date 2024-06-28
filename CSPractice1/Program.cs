a21426b50d8d952f2a41ba1f632562f598023be0
Darecoco <whaqlrh37@naver.com>
2024-06-28 10:06:27 +0900

    #5 14-6 foreach 반복문

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -130,6 +130,13 @@ namespace CSPractice1
                 Console.WriteLine(intArray2[i]);
                 i++; // 탈출을 위한 변수
             }
+
+            // #5 14-6 foreach 반복문
+            string[] foreachArray = { "사과", "배", "포도" };
+            foreach (var item in foreachArray)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
